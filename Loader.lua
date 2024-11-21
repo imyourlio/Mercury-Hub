74f802e046eef05ef62a66d945ecf6c49062d462
imyourlio <lrautvuori@gmail.com>
2024-11-22 05:20:26 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,10 +1,8 @@
 local PlaceId = tostring(game.PlaceId)
 
-if LRM_IsUserPremium then
-    if PlaceId == "16732694052" then
-        loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/d9bcdd52f5f26dab5b47acce64c74962.lua"))()
-        return
-    else
-        print("Wrong game")
-    end
+if PlaceId == "16732694052" then
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/d9bcdd52f5f26dab5b47acce64c74962.lua"))()
+    return
+else
+    print("Wrong game")
 end
