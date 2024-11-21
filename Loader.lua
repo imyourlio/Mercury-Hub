1c8ef8247fa7f25fbb961130b4a09fe2054b10bc
imyourlio <lrautvuori@gmail.com>
2024-11-22 03:39:17 +0700

    Create Loader.lua

---
+++ Loader.lua
@@ -0,0 +1,8 @@
+local PlaceId = tostring(game.PlaceId)
+
+if PlaceId == "16732694052" then
+        loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/7ab8c76dd4db54f50cc12f12c92f72d8.lua"))()
+    return
+else
+    print("Wrong game")
+end
