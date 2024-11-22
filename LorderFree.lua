df2bb5e50c83a596db22cf24a761cec0d246857d
imyourlio <lrautvuori@gmail.com>
2024-11-22 20:38:54 +0700

    Update LorderFree.lua

--- LorderFree.lua
+++ LorderFree.lua
@@ -1,5 +1,7 @@
 local PlaceId = tostring(game.PlaceId)
 
+script_key = getgenv().License
+
 if PlaceId == "16732694052" then
     loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/7ab8c76dd4db54f50cc12f12c92f72d8.lua"))()
     return
