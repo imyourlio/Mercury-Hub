c24a4069ef2861e873f340f1b0a505a3e8d5a7bd
imyourlio <lrautvuori@gmail.com>
2024-12-01 03:32:11 +0700

    Delete LoaderPrem.lua

--- LoaderPrem.lua
+++
@@ -1,8 +0,0 @@
-local PlaceId = tostring(game.PlaceId)
-
-if PlaceId == "16732694052" then
-    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/d9bcdd52f5f26dab5b47acce64c74962.lua"))()
-    return
-else
-    print("Wrong game")
-end
