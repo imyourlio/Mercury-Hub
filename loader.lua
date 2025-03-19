77947f1e9718c5558eb2f824bf6b79ac63fabc03
imyourlio <lrautvuori@gmail.com>
2025-03-19 18:14:19 +0700

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,9 @@
+local PlaceId = tostring(game.PlaceId)
+
+if PlaceId == "16732694052" then -- Fisch
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/22c5041946b8240342a41f22f2089609.lua"))()
+elseif PlaceId == "18668065416" then -- Blue Lock
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/22c5041946b8240342a41f22f2089609.lua"))()
+elseif PlaceId == "116495829188952" then -- Dead Rails
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/a0b780c1674cdcff3f246bfb634022cf.lua"))()
+end
