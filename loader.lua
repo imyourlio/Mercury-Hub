e1f9b4d47f625f746739346579032baa9f68d981
imyourlio <lrautvuori@gmail.com>
2025-03-26 11:24:01 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -6,4 +6,6 @@ elseif PlaceId == "18668065416" then -- Blue Lock
     loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/22c5041946b8240342a41f22f2089609.lua"))()
 elseif PlaceId == "116495829188952" then -- Dead Rails
     loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/a0b780c1674cdcff3f246bfb634022cf.lua"))()
+elseif PlaceId == "87039211657390" or PlaceId == "128336380114944" then -- Arise Crossover
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/56cf02382eb92068e6d290bf1ea3d36f.lua"))()
 end
