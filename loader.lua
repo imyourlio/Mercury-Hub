39fd05fc76370c47a1f082c40aafc430bc5aa577
imyourlio <lrautvuori@gmail.com>
2025-03-30 05:52:54 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,6 +1,6 @@
 local PlaceId = tostring(game.PlaceId)
 
-if PlaceId == "16732694052" then -- Fisch
+if PlaceId == "16732694052" or PlaceId == "72907489978215" then -- Fisch
     loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/22c5041946b8240342a41f22f2089609.lua"))()
 elseif PlaceId == "18668065416" then -- Blue Lock
     loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/22c5041946b8240342a41f22f2089609.lua"))()
